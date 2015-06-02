a10682460553182dd9d6cea68d1d0415114421a9
lucasjo <lucasjo@mail.uc.edu>
2015-06-02 19:09:48 -0400

    YEAH BITCHES
    Movement is better

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,18 +4,26 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 	public Vector2 speed = new Vector2(50, 50);
 	private Vector2 movement;
-	public float inputX = 0f;
+	public float inputX = 0.0f;
 	public float inputY = 0.0f;
 
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.W))
-			inputY += 1;
+			inputY += .5f;
+		if (Input.GetKey(KeyCode.W) && inputY < 2)
+			inputY += .1f;
 		if (Input.GetKeyDown(KeyCode.S))
-			inputY -= 1;
+			inputY -= .5f;
+		if (Input.GetKey(KeyCode.S) && inputY > -2)
+			inputY -= .1f;
 		if (Input.GetKeyDown(KeyCode.A))
-			inputX -= 1;
+			inputX -= .5f;
+		if (Input.GetKey(KeyCode.A) && inputX > -2)
+			inputX -= .1f;
 		if (Input.GetKeyDown(KeyCode.D))
-			inputX += 1;
+			inputX += .5f;
+		if (Input.GetKey(KeyCode.D) && inputX < 2)
+			inputX += .1f;
 		
 		movement = new Vector2(
 			speed.x * inputX,
