1c93b55ae90f48cd6b03237b2afb8a5e2e7232e6
lucasjo <lucasjo@mail.uc.edu>
2015-06-02 18:53:44 -0400

    You can move very badly
    Added wasd movement bitched

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public Vector2 speed = new Vector2(50, 50);
+	private Vector2 movement;
+	public float inputX = 0f;
+	public float inputY = 0.0f;
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.W))
+			inputY += 1;
+		if (Input.GetKeyDown(KeyCode.S))
+			inputY -= 1;
+		if (Input.GetKeyDown(KeyCode.A))
+			inputX -= 1;
+		if (Input.GetKeyDown(KeyCode.D))
+			inputX += 1;
+		
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
