39111b4cb3f9c9707c8a49f0cf37a939ffbfdcbb
lucasjo <lucasjo@mail.uc.edu>
2015-06-03 23:09:21 -0400

    hope this works

--- Assets/Scripts/Space.cs
+++ Assets/Scripts/Space.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class Space : MonoBehaviour {
 
+	public Transform food;
+
 	// Use this for initialization
 	void Start () {
-	
+		var foodTransform = Instantiate(food) as Transform;
+		
+		foodTransform.position = new Vector3(Random.Range(-8,8), Random.Range(-5,5), 5);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+//		var foodTransform = Instantiate(food) as Transform;
+//
+//		foodTransform.position = new Vector3(Random.Range(1,1680), Random.Range(1,1258), 5);
+//
+//		Debug.Log ("DERPDEDERPHERPDERPDERP");
+
 	}
 }
