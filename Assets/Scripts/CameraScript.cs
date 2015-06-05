c6083b3c3960bb51f8ef9021ab2f79bddd13f9ce
lucasjo <lucasjo@mail.uc.edu>
2015-06-05 00:27:10 -0400

    Camera Zoom
    zoom is set to be 50 * player scale... if we want to make it smaller
    then we can also adjust player scale to make the player smaller to make
    the level larger now that the camera zooms

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	private GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Player = GameObject.FindGameObjectsWithTag("Player")[0];
+
+		if (Player != null) {
+
+			Camera.main.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -10);
+			
+			Camera.main.orthographicSize = Player.transform.localScale.x * 50;
+
+		}
+	}
+}
