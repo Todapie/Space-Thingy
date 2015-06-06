5cd7a8d2a136c0f40696c53b8ac768b167862ab1
CodyHowell <codyhowell7@gmail.com>
2015-06-05 23:40:22 -0400

    Working on networking

--- Assets/Scripts/Network/PhotonManagerScript.cs
+++ Assets/Scripts/Network/PhotonManagerScript.cs
@@ -1,15 +1,25 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
+	const string VERSION = "v0.1";
+	public string roomName = "Test Room";
+	public string playerPrefabname = "player";
+	public Transform spawnPoint;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		PhotonNetwork.ConnectUsingSettings (VERSION);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnJoinedLobby()
+	{
+		RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
+		PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+	}
+	void OnJoinedRoom()
+	{
+		PhotonNetwork.Instantiate (playerPrefabname, spawnPoint.position, spawnPoint.rotation, 0);
 	}
 }
+*/
