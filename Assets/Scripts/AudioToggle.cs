417d275ac0837400872a69dd99723ea416ebdba9
Jtholland4 <jthbaseball4@gmail.com>
2015-06-12 03:16:11 -0400

    Added music to our menu screen for shits and giggles
    also placed an object in the scene that will be used for server
    communication.

---
+++ Assets/Scripts/AudioToggle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioToggle : MonoBehaviour {
+	
+	public void AudioToggleCheck(bool Toggle) {
+		if (Toggle) {
+			if (GetComponent<AudioSource> ().isPlaying) {
+				GetComponent<AudioSource> ().Stop ();
+			} else {
+				GetComponent<AudioSource> ().Play ();
+			}
+		}
+	}
+}
